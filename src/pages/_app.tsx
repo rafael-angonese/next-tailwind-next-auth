289cb1ff8a7b9176eba1cfc90bd397127eb82224
Rafael Angonese <rafael.angonese09@gmail.com>
2021-06-27 17:27:08 -0300

    config tailwind css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app';
 import { Provider } from 'next-auth/client';
 
+import '../styles/tailwind.css';
+// import 'tailwindcss/tailwind.css'
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
