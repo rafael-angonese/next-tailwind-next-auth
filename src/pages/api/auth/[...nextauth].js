3c69b34ebc24f189440c4e5c30036232a09058b4
Rafael Angonese <rafael.angonese09@gmail.com>
2021-06-27 16:58:06 -0300

    next auth github provider

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,15 +2,19 @@ import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
 export default NextAuth({
-    // Configure one or more authentication providers
     providers: [
         Providers.GitHub({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET
         })
-        // ...add more providers here
     ],
 
-    // A database is optional, but required to persist accounts in a database
-    database: process.env.DATABASE_URL
+    session: {
+        jwt: true,
+        maxAge: 30 * 24 * 60 * 60 // 30 days
+    },
+
+    jwt: {
+        secret: process.env.SECRET
+    }
 });
